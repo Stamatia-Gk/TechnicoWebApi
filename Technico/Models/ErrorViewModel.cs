8670b0a82ebdbc9f1ee13e1f65ad75e45f1b5fcf
Stamatia-Gk <stamatia.gkouma@gmail.com>
2024-11-13 18:33:55 +0200

    Created MVC

---
+++ Technico/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Technico.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
