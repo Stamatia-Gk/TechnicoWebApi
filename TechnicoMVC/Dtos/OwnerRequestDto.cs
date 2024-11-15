d147737a3b86facc40c0196f6ca16c9bbf6de2e6
Stamatia-Gk <107853450+Stamatia-Gk@users.noreply.github.com>
2024-11-15 13:29:06 +0200

    Delete TechnicoMVC directory

--- TechnicoMVC/Dtos/OwnerRequestDto.cs
+++
@@ -1,8 +0,0 @@
-﻿// Team Project | European Dynamics | Code.Hub Project 2024
-
-namespace TechnicoWebApi.Dtos;
-
-public class OwnerRequestDto : OwnerResponseDto
-{
-    public string Password { get; set; } = string.Empty;
-}
--- TechnicoMVC/Dtos/OwnerWithPropertiesResponseDto.cs
+++
@@ -1,8 +0,0 @@
-﻿// Team Project | European Dynamics | Code.Hub Project 2024
-
-namespace TechnicoWebApi.Dtos;
-
-public class OwnerWithPropertiesResponseDto: OwnerResponseDto
-{
-    public List<PropertyDto>? OwnerProperties { get; set; }
-}
