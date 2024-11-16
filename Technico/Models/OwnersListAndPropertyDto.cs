6e1b186915911f4313400f04c500592c18401b86
Stamatia-Gk <stamatia.gkouma@gmail.com>
2024-11-16 18:58:05 +0200

    fixed bug @Testing stuff for owner bind when creating a property.

---
+++ Technico/Models/OwnersListAndPropertyDto.cs
@@ -0,0 +1,11 @@
+﻿
+using TechnicoWebApi.Dtos;
+using TechnicoWebApi.Models;
+
+namespace Technico.Models;
+
+public class OwnersListAndPropertyDto : PropertyDto
+{
+    public List<Owner> Owners { get; set; } = new();
+    public PropertyDto Property { get; set; }
+}
